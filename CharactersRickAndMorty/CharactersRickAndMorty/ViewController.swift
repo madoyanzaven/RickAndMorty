5bf73c73cb9c75207977c3e67b9f84c3bdd74555
Zaven Madoyan <madoyanzaven@gmail.com>
2023-01-16 16:42:16 +0400

    add project

---
+++ CharactersRickAndMorty/CharactersRickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CharactersRickAndMorty
+//
+//  Created by Zaven Madoyan on 16.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
